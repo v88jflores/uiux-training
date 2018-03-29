4b0f59ca79504f52e0783a2cd16d96dedcf55a63
v88jflores <jflores@village88.com>
2018-03-29 18:02:26 +0800

    Upload output in web animations (css transition)

--- index.php
+++ index.php
@@ -2,10 +2,20 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title><Document></Document></title>
+	<title>Home | UI/UX Training</title>
+	<style>
+		body{
+			padding: 60px 40px;
+		}
+		a{
+			display: block;
+			font-size: 18px;
+			padding: 5px 0px;
+		}
+	</style>
 </head>
 <body>
-	<h1>Hello Philippines!</h1>
-	<a href="/pages/jungle_salad/">Jungle Salad</a>
+	<a href="/pages/jungle_salad/" target="_blank">Jungle Salad</a>
+	<a href="/pages/web_animation/css_transition" target="_blank">Web Animation | CSS Transitions</a>
 </body>
 </html>
---
+++ pages/web_animation/css_transition/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CSS Transition | UI/UX Training</title>
+
+	<link rel="stylesheet" href="/assets/web_animation/css_transition/css_transition.css">
+</head>
+<body>
+	<div class="video_output">
+		<h1>Recreated the video's expected output</h1>
+		<div>
+			<img src="https://cutecdn.codingdojo.com/new_design_image/global/sprite_images/stacks_iconography.png" alt="">
+		</div>
+	</div>
+	<div class="own_concept">
+		<h1>CSS Transition (Own Concept)</h1>
+		<div class="transition_1">
+			<p>Hover Me!</p>
+		</div>
+		<div class="transition_2">
+			<p>Hover Me!</p>
+		</div>
+		<div class="transition_3">
+			<p>Hover Me!</p>
+		</div>
+		<div class="transition_4">
+			<input type="text" class="full_name" placeholder="Full Name">
+		</div>
+		<div class="transition_5">
+			<input type="text" class="full_name" placeholder="Full Name">
+			<div class="border_bottom"></div>
+		</div>
+		<div class="transition_6">
+			<label for="radio_option_1">
+				<input id="radio_option_1" type="radio" name="radio_options">
+				<div class="checkbox_marker">
+					<div class="checkbox_indicator"></div>
+				</div>
+				<div class="option_label">
+					<span>Option 1</span>
+					<span class="label_border"></span>
+				</div>
+			</label>
+			<label for="radio_option_2">
+				<input id="radio_option_2" type="radio" name="radio_options">
+				<div class="checkbox_marker">
+					<div class="checkbox_indicator"></div>
+				</div>
+				<div class="option_label">
+					<span>Option 2</span>
+					<span class="label_border"></span>
+				</div>
+			</label>
+			<label for="radio_option_3">
+				<input id="radio_option_3" type="radio" name="radio_options">
+				<div class="checkbox_marker">
+					<div class="checkbox_indicator"></div>
+				</div>
+				<div class="option_label">
+					<span>Option 3</span>
+					<span class="label_border"></span>
+				</div>
+			</label>
+		</div>
+	</div>
+</body>
+</html>
